761fc3b823a7dd61594b750590535843742de2b7
ka <ka.kaosf@gmail.com>
2013-06-26 01:31:26 +0900

    add ibus setup processes

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ sudo aptitude safe-upgrade
 # Can I do this with CLI?
 
 sudo aptitude install zsh vim tmux curl git tig
+
+# ref.
+#  http://kaiyanotako.blogspot.jp/2012/07/lubuntu-1204-mozc.html
+#  http://ysnmdm.air-nifty.com/blog/2012/10/lubuntumozc-1e1.html
+sudo aptitude install ibus-mozc python-appindicator ibus-gtk ibus-gtk3
