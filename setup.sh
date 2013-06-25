dd487ad151fbecbbaba27e38f3394e9846a0f31b
ka <ka.kaosf@gmail.com>
2013-06-26 01:28:06 +0900

    add setup.sh
    setup CLI environment mainly

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install aptitude
+sudo aptitude update
+sudo aptitude safe-upgrade
+
+# install "Additinal drivers" with Lubuntu Software Center
+# Can I do this with CLI?
+
+sudo aptitude install zsh vim tmux curl git tig
