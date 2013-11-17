590baac4857d5143d5fa8656d40ce6dd51f511b7
ka <ka.kaosf@gmail.com>
2013-11-17 13:50:54 +0900

    install packages for sound

--- setup.sh
+++ setup.sh
@@ -11,3 +11,6 @@ sudo aptitude install zsh vim tmux curl git tig
 #  http://kaiyanotako.blogspot.jp/2012/07/lubuntu-1204-mozc.html
 #  http://ysnmdm.air-nifty.com/blog/2012/10/lubuntumozc-1e1.html
 sudo aptitude install ibus-mozc python-appindicator ibus-gtk ibus-gtk3
+
+# ref. http://ubuntuforums.org/showthread.php?t=1969564
+sudo aptitude install pavucontrol pulseaudio
