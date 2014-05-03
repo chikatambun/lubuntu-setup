67d6568dd9cb3b758966fed6aacb4c4c38c37628
ka <ka.kaosf@gmail.com>
2014-05-03 16:07:21 +0900

    Remove tmux from installed packages list

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo aptitude safe-upgrade
 # install "Additinal drivers" with Lubuntu Software Center
 # Can I do this with CLI?
 
-sudo aptitude install zsh vim tmux curl git tig
+sudo aptitude install zsh vim curl git tig
 
 # ref.
 #  http://kaiyanotako.blogspot.jp/2012/07/lubuntu-1204-mozc.html
