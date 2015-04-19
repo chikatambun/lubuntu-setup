a1f456281fade7eeec8dc3d4f13b1b2735cac105
ka <ka.kaosf@gmail.com>
2015-04-19 11:50:20 +0900

    Modify keyboard delay on Lubuntu from 220 to 200

--- keyboard-delay-interval-setting.sh
+++ keyboard-delay-interval-setting.sh
@@ -1,4 +1,4 @@
 CONFIGFILE=$HOME/.config/lxsession/Lubuntu/desktop.conf
-sed 's/Delay=500/Delay=220/' -i $CONFIGFILE
+sed 's/Delay=500/Delay=200/' -i $CONFIGFILE
 sed 's/Interval=30/Interval=10/' -i $CONFIGFILE
 touch $CONFIGFILE
